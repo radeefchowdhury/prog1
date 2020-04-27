7f78d926fc91577d6ec0c2d75c4c309ff351c513
radeefchowdhury <60351584+radeefchowdhury@users.noreply.github.com>
2020-04-27 10:54:51 -0400

    Update scan.java

--- scan.java
+++ scan.java
@@ -1,17 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
-/**
- *
- * @author radee
- */
-class scan {
+public class ReadingFile {
 
-    static boolean nextLine() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   // This programs reads a file with names and scores, displaying the names
+   //highest and lowest score, total amount of grades and the average of all the scores 
+    // I couldn't get netbeans to read my file when I entered integers
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\radee\\Desktop\\sathu.txt");
+        Scanner scFile = new Scanner(file);
+        while(scFile.hasNext()){
+            String names = scFile.next();
+            scFile.useDelimiter(" "); 
+            System.out.println(names);
+            int score = scFile.nextInt();
+            System.out.println(score);
+            
+            
+        }
+        
     }
     
 }
